091200b8e4652786ad1bb8acb11bc25f76d5cc87
Junior <Junior@LAPTOP-LHL01L2S>
2022-02-24 12:51:16 -0300

    Adicionar arquivos de projeto.

---
+++ NhibernateTeste/Interface/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace NhibernateTeste.Interface
+{
+    public interface IRepository<T>
+    {
+        Task Add(T item);
+        Task Remove(long id);
+        Task Update(T item);
+        Task<T> FindByID(long id);
+        IEnumerable<T> FindAll();
+    }
+
+}
---
+++ NhibernateTeste/Program.cs
@@ -0,0 +1,55 @@
+using NHibernate.Cfg;
+using NhibernateTeste.Extension;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connStr = "Data Source=Products.db";
+
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddNHibernate(connStr);
+
+
+
+
+
+
+var app = builder.Build();
+
+
+   
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
